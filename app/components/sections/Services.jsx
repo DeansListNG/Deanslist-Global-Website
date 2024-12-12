15ee71331e084fdb79e9304c8fa8de4d830f4bd8
Baribor Saturday <saturdaybaribor@gmail.com>
2024-12-12 12:49:26 +0100

    fix services ui section

--- app/components/sections/Services.jsx
+++ app/components/sections/Services.jsx
@@ -52,13 +52,8 @@ const Services = () => {
 							Our goal is to make your product user-friendly with top-notch functionality <br /> that meets industry standards.
 						</p>
 					</div>
-					<div className="grid grid-cols-[30%,1fr] w-full max-h-[200px] items-baseline">
-						<div className="">
-							<img src="/images/feedback1.svg" alt="feedback" />
-						</div>
-						<div>
-							<img src="/images/feedback2.svg" alt="feedback" className="w-full" />
-						</div>
+					<div className="w-full max-h-[200px] items-baseline">
+						<img src="/images/feedback-review.svg" alt="feedback review" className="w-full" />
 					</div>
 
 				</div> 
@@ -91,17 +86,7 @@ const Services = () => {
 						</p>
 						<div className="flex relative justify-center items-center flex-grow h-[200px]">
 							{/* Hackathon Badges */}
-							<div className="bg-gradient-to-r from-gray-700 to-gray-500 md:text-[16px] translate-y-3 md:translate-y-0 text-[12px] md:px-4 px-[10px] py-[6px] md:py-2 rounded-md absolute top-0 left-0">
-								Renaissance Hackathon
-							</div>
-							<div className="bg-gradient-to-r from-[#D29808] to-[#8B680C] border border-[#D29808] translate-y-3 md:translate-y-0 md:text-[16px] text-[12px] md:px-4 px-[10px] py-[6px] rounded-md absolute top-12 right-0">
-								Radar Hackathon
-							</div>
-							<div className="bg-black md:text-[16px] text-[12px] md:px-4 px-[10px] py-[6px] -translate-y-2 md:translate-y-0 rounded-md absolute bottom-0 border border-white text-sm">
-								Hyperdrive Hackathon
-							</div>
-
-							<img src="/solana.png" alt="solana" className="w-24 h-24 p-4 rounded-full bg-black" />
+							<img src="/images/hackathon-judging.svg" alt="hackathon judging" />
 						</div>
 					</div>
 
@@ -114,11 +99,9 @@ const Services = () => {
 							Get real-time, in-depth reviews tailored to your project. Our live feedback sessions deliver actionable
 							insights to help you refine and elevate your product.
 						</p>
-						<div className="relative w-full h-full grid md:grid-cols-[30%,1fr]">
-							<div className="relative hidden md:block">
-								<span className="bg-[#673882] p-2 px-3 rounded-full border border-purple-600 absolute top-2 text-sm">Bug fixing</span>
-								<span className="bg-[#673882] p-2 px-3 rounded-full border border-purple-600 absolute text-sm right-2 top-[35%]">Live testing</span>
-								<span className="bg-[#673882] p-2 px-3 rounded-full border border-purple-600 absolute bottom-8 left-2 text-sm">UI Improvements</span>
+						<div className="relative w-full h-full grid grid-cols-[30%,1fr] gap-1">
+							<div className="py-2">
+								<img src="/images/feedback-bonanza.svg" alt="feedback bonanza" />
 							</div>
 							<div className=" w-full h-full">
 								<ReactPlayer url="https://youtu.be/ILuvSo5gw6w" width="100%" height="100%" />
