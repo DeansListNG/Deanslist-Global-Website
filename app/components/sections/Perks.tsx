a44ef4b110b1f296fe16ebfe9d31577384c20606
Abasiakan Mbat <basky2592@gmail.com>
2025-09-16 22:49:25 +0100

    added perks section

---
+++ app/components/sections/Perks.tsx
@@ -0,0 +1,66 @@
+import { perkData } from "@/app/data/perks-data"
+import { Card } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  DotButtons,
+} from "@/components/ui/carousel"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+
+export default function Perks() {
+  return (
+    <div className="flex flex-col items-center">
+     
+      <Card className="xl:w-[1275px] xl:max-w-[1275px]  md:max-w-[768px] sm:w-[768px] w-full    pt-0 border-none shadow-none relative mx-10 bg-transparent">
+        <Carousel  className="xl:w-[1275px] xl:max-w-[1275px]  md:max-w-[768px] sm:w-[768px] w-full     mx-auto border-none py-5">
+          <CarouselContent>
+            {perkData.map((perk, index) => (
+              <CarouselItem key={index}>
+
+                <PerksCard>
+
+                  <div className="relative  flex items-center justify-center">
+                    <div className="absolute w-28 top-0 left-1   h-40 rounded-full bg-white blur-xl  -rotate-45 opacity-5"></div>
+                    <div className="absolute w-28 top-0 right-1   h-40 rounded-full bg-white blur-xl  rotate-45 opacity-5"></div>
+                    <div className=" mx-auto w-fit ">
+                      <Image src={perk.image} alt="Perk" width={116} height={116} />
+                      <p className="text-center text-[#ABD6B3]">{perk.name}</p>
+                    </div>
+                  </div>
+                  <div className="relative w-full  space-y-4 text-center  border-[#fff]  ">
+                    <div className="bg-[#103E3E] blur-[2px] w-full absolute top-0 h-full"> </div>
+                    <div className="absolute top-0 w-full  space-y-2">
+
+                      <h2 className="text-preset-4 text-white">{perk.title}</h2>
+                      <p className="max-w-[300px] text-wrap mx-auto text-[#C0C3CE] text-center text-preset-4-slim">{perk.descriptions}</p>
+                    </div>
+                  </div>
+                </PerksCard>
+
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+
+          <CarouselPrevious />
+          <CarouselNext />
+          <DotButtons className="mt-4" />
+        </Carousel>
+      </Card>
+
+      {/* ✅ Now automatically connected to carousel via context */}
+    </div>
+  )
+}
+
+
+export function PerksCard({ className, children }: { className?: string, children?: React.ReactNode }) {
+  return (
+    <div className={cn("grid grid-rows-[60%_auto]", className, "  xl:h-[348.24px] md:h-[314.24px] h-[314.24px] w-full  xl:w-[421px] max-w-full rounded-xl bg-perkBg border border-perkBorder")}>
+      {children}
+    </div>
+  )
+}
---
+++ app/data/perks-data.ts
@@ -0,0 +1,22 @@
+
+interface perrkDataTypes {
+  id: number;
+  name: string;
+  image: string;
+  title: string;
+  descriptions: string;
+}
+export const perkData: perrkDataTypes[] = [
+  { id: 1, name: "Dragon", image: "/perks/dragon.svg", title: "Eligibility for Merch Bags", descriptions: "Qualify to apply for limited official merch bags. First-come, first-served basis, priced in $USDC with worldwide shipping." },
+  { id: 2, name: "Goat", image: "/perks/goat.svg", title: "Special Event Bracelet", descriptions: "Receive exclusive event bracelet featuring your name, QR code linked to socials, and personalized details." },
+  { id: 3, name: "Horse", image: "/perks/horse.svg", title: "1% Perks Leaderboard Boost", descriptions: "Enjoy 1% boost to base Perks Leaderboard score. Stackable percentages for multiple holdings." },
+  { id: 4, name: "Inu", image: "/perks/inu.svg", title: "Priority Airdrops & Whitelist", descriptions: "Receive priority consideration for airdrops and whitelists from IslandDAO partner projects and communities." },
+  { id: 5, name: "Monkey", image: "/perks/monkey.svg", title: "Featured Extra in Event Videos", descriptions: "Be prominently featured as extras in video content produced during real-life events by IslandDAO." },
+  { id: 6, name: "Mouse", image: "/perks/mouse.svg", title: "Cardboard Cutout Eligibility", descriptions: "Apply for limited spots for life-sized or tabletop cardboard cutouts at IslandDAO events. Nominal $USDC fee required." },
+  { id: 7, name: "Ox", image: "/perks/ox.svg", title: "DAO Governance Multiplier", descriptions: "Gain 1% multiplier boost to governance token voting power. Stackable benefits for multiple NFTs." },
+  { id: 8, name: "Pig", image: "/perks/pig.svg", title: "Plus One at Events", descriptions: "Receive the privilege of bringing a plus one guest to IslandDAO events." },
+  { id: 9, name: "Rabbit", image: "/perks/rabbit.svg", title: "Complimentary Coffee & Early Access", descriptions: "Enjoy complimentary premium coffee at IRL events and exclusive early access to premium IslandDAO dApp features." },
+  { id: 10, name: "Rooster", image: "/perks/rooster.svg", title: "Private Q&A with Core Contributors", descriptions: "Gain access to private, recurring Q&A sessions with IslandDAO core contributors for direct engagement." },
+  { id: 11, name: "Snake", image: "/perks/snake.svg", title: "Service Discounts", descriptions: "Benefit from 0.2% flat discount on all DAO products and services. Stackable discounts for multiple holdings." },
+  { id: 12, name: "Tiger", image: "/perks/tiger.svg", title: "Special Event Shoutouts", descriptions: "Receive special recognition and shoutouts during IslandDAO events. Community can modify perks for holder benefit." },
+]
